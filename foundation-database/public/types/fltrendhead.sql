9f947de68d2c23a4f8164ce8e3abde5d8847610e
Chris Cotnoir <ccotnoir@xtuple.com>
2018-04-11 18:57:57 -0400

    Move (most) types out of 440_schema

---
+++ foundation-database/public/types/fltrendhead.sql
@@ -0,0 +1,20 @@
+DROP TYPE IF EXISTS fltrendhead CASCADE;
+CREATE TYPE fltrendhead AS (
+        fltrendhead_flhead_id integer,
+        fltrendhead_username text,
+        fltrendhead_typedescrip text,
+        fltrendhead_flhead_name text,
+        fltrendhead_fld1 text,
+        fltrendhead_fld2 text,
+        fltrendhead_fld3 text,
+        fltrendhead_fld4 text,
+        fltrendhead_fld5 text,
+        fltrendhead_fld6 text,
+        fltrendhead_fld7 text,
+        fltrendhead_fld8 text,
+        fltrendhead_fld9 text,
+        fltrendhead_fld10 text,
+        fltrendhead_fld11 text,
+        fltrendhead_fld12 text,
+        fltrendhead_grndttl text
+);
