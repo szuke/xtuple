7698d0dc1094842b5c54a549e7e3943186a037c6
Dave Anderson (xTuple) <danderson@xtuple.com>
2018-07-30 13:57:11 +1200

    [24325] Prevent zero (or less) Vendor UOM ratio in Item Source record

--- foundation-database/public/trigger_functions/itemsrc.sql
+++ foundation-database/public/trigger_functions/itemsrc.sql
@@ -1,36 +1,40 @@
-CREATE OR REPLACE FUNCTION _itemsrcTrigger () RETURNS TRIGGER AS '
--- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
+CREATE OR REPLACE FUNCTION _itemsrcTrigger () RETURNS TRIGGER AS $$
+-- Copyright (c) 1999-2018 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 BEGIN
 
 -- Privilege Checks
-   IF (NOT checkPrivilege(''MaintainItemSources'')) THEN
-     RAISE EXCEPTION ''You do not have privileges to maintain Item Sources.'';
+   IF (NOT checkPrivilege('MaintainItemSources')) THEN
+     RAISE EXCEPTION 'You do not have privileges to maintain Item Sources.';
    END IF;
 
 -- Set defaults
-   NEW.itemsrc_invvendoruomratio	:= COALESCE(NEW.itemsrc_invvendoruomratio,1);
-   NEW.itemsrc_minordqty		:= COALESCE(NEW.itemsrc_minordqty,0);
-   NEW.itemsrc_multordqty		:= COALESCE(NEW.itemsrc_multordqty,0);
-   NEW.itemsrc_active			:= COALESCE(NEW.itemsrc_active,true);
-   NEW.itemsrc_leadtime			:= COALESCE(NEW.itemsrc_leadtime,0);
-   NEW.itemsrc_ranking			:= COALESCE(NEW.itemsrc_ranking,1);
-  
-  RETURN NEW;
-END;
-' LANGUAGE 'plpgsql';
+   NEW.itemsrc_invvendoruomratio := COALESCE(NEW.itemsrc_invvendoruomratio,1);
+   NEW.itemsrc_minordqty         := COALESCE(NEW.itemsrc_minordqty,0);
+   NEW.itemsrc_multordqty        := COALESCE(NEW.itemsrc_multordqty,0);
+   NEW.itemsrc_active            := COALESCE(NEW.itemsrc_active,true);
+   NEW.itemsrc_leadtime          := COALESCE(NEW.itemsrc_leadtime,0);
+   NEW.itemsrc_ranking           := COALESCE(NEW.itemsrc_ranking,1);
 
-SELECT dropifexists('TRIGGER', 'itemsrcTrigger');
+   IF (NEW.itemsrc_invvendoruomratio <= 0) THEN
+      NEW.itemsrc_invvendoruomratio = 1;
+   END IF;
+
+   RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS itemsrctrigger ON public.itemsrc;
 CREATE TRIGGER itemsrcTrigger BEFORE INSERT OR UPDATE ON itemsrc FOR EACH ROW EXECUTE PROCEDURE _itemsrcTrigger();
 
-CREATE OR REPLACE FUNCTION _itemsrcAfterTrigger () RETURNS TRIGGER AS '
--- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
+CREATE OR REPLACE FUNCTION _itemsrcAfterTrigger () RETURNS TRIGGER AS $$
+-- Copyright (c) 1999-2018 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 BEGIN
 
 -- Privilege Checks
-  IF (NOT checkPrivilege(''MaintainItemSources'')) THEN
-    RAISE EXCEPTION ''You do not have privileges to maintain Item Sources.'';
+  IF (NOT checkPrivilege('MaintainItemSources')) THEN
+    RAISE EXCEPTION 'You do not have privileges to maintain Item Sources.';
   END IF;
 
 -- Set default to false for other item sources of this item
@@ -42,7 +46,7 @@ BEGIN
 
   RETURN NEW;
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE plpgsql;
 
-SELECT dropIfExists('trigger', 'itemsrcAfterTrigger');
+DROP TRIGGER IF EXISTS itemsrcAfterTrigger  ON public.itemsrc;
 CREATE TRIGGER itemsrcAfterTrigger AFTER INSERT OR UPDATE ON itemsrc FOR EACH ROW EXECUTE PROCEDURE _itemsrcAfterTrigger();
