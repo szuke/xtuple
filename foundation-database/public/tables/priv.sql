e350c2001c2855e84a0e9d1e69b15b53a34dbc7e
Dave Anderson <dave@pentuple.co.nz>
2018-08-15 12:09:33 +1200

    [4474] Add privilege for editing customer/prospect numbers

--- foundation-database/public/tables/priv.sql
+++ foundation-database/public/tables/priv.sql
@@ -21,7 +21,8 @@ declare
       ['Purchase', 'MaintainPurchaseTypes', 'Can Maintain Purchase Types'],
       ['Purchase', 'ViewPurchaseTypes', 'Can View Purchase Types'],
       ['Accounting', 'ReverseARApplication', 'Allows A/R Applications to be reversed'],
-      ['Accounting', 'ReverseAPApplication', 'Allows A/P Applications to be reversed']
+      ['Accounting', 'ReverseAPApplication', 'Allows A/P Applications to be reversed'],
+      ['Sales', 'MaintainCustomerNumbers', 'Can edit or update Customer or Prospect numbers']
     ];
   _p TEXT[];
 begin
