63b45861c9b3ba3b7afb1fe24e715919dc16733d
Michael Micros <mmicros@xtuple.com>
2018-10-12 13:05:55 -0400

    removing 'IF EXISTS' from 'DROP FUNCTION'

--- foundation-database/public/functions/createarcreditmemo.sql
+++ foundation-database/public/functions/createarcreditmemo.sql
@@ -1,4 +1,4 @@
-SELECT DROP FUNCTION IF EXISTS createarcreditmemo(integer, integer, text, text, date, numeric, text, integer, integer, integer, date, integer, integer, numeric, integer, integer, integer, integer, integer);
+SELECT DROP FUNCTION createarcreditmemo(integer, integer, text, text, date, numeric, text, integer, integer, integer, date, integer, integer, numeric, integer, integer, integer, integer, integer);
 
 CREATE OR REPLACE FUNCTION createARCreditMemo(pId            INTEGER,
                                               pCustid        INTEGER,
