2a0d42d8e1bcd908137c25777d01ddeac2aa624f
Michael Micros <mmicros@xtuple.com>
2018-10-12 13:28:33 -0400

    forgot something

--- foundation-database/public/functions/createardebitmemo.sql
+++ foundation-database/public/functions/createardebitmemo.sql
@@ -1,4 +1,4 @@
-DROP FUNCTION createardebitmemo(integer, integer, integer, text, text, date, numeric, text, integer, integer, integer, date, integer, integer, numeric, integer);
+DROP FUNCTION IF EXISTS createardebitmemo(integer, integer, integer, text, text, date, numeric, text, integer, integer, integer, date, integer, integer, numeric, integer);
 
 CREATE OR REPLACE FUNCTION createARDebitMemo( pId            INTEGER,
                                               pCustid        INTEGER,
