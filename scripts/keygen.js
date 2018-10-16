2851630025fa9ed8d2715097283b8382938d957e
Andrey Mikheychik <amikheychik@xtuple.com>
2018-10-16 07:24:31 -0400

    Separate promises for common functions and keygen logic

--- scripts/keygen.js
+++ scripts/keygen.js
@@ -65,49 +65,59 @@
   /** @type {{psi: Object, asn1: Object, pkcs12: Object}} */
   let forge = require('node-forge');
   let filesystem = require('fs');
-  let Promises = {
-    createKeypair: function (resolve, reject) {
-      forge.pki.rsa.generateKeyPair(2048, 65537, null, (error, keypair) => {
-        if (error) {
-          reject(error);
-        }
-        resolve(keypair);
-      });
-    },
-    readOAuth2ClientSQLFile: function (resolve, reject) {
-      filesystem.readFile(
-        '{directory}/sql/oauth2client.sql'.replace('{directory}', __dirname),
-        'utf8',
-        (error, content) => {
+  let promises = {
+    /**
+     * @returns {Promise}
+     */
+    generateRSAKeyPair: function (bits, exponent) {
+      return new Promise((resolve, reject) => {
+        forge.pki.rsa.generateKeyPair(bits, exponent, null, (error, keypair) => {
           if (error) {
             reject(error);
           }
-          resolve(content);
-        }
-      );
+          resolve(keypair);
+        });
+      });
     },
-    saveP12File: function (privateKey) {
-      return (resolve, reject) => {
-        filesystem.writeFile(script.path, new Buffer(new Buffer(
-          forge.asn1.toDer(
-            forge.pkcs12.toPkcs12Asn1(privateKey, null, 'notasecret')
-          ).getBytes(),
-          'binary'
-        ), 'base64'), (error, result) => {
+    /**
+     * @param {string} path
+     * @returns {Promise}
+     */
+    readFile: function (path) {
+      return new Promise((resolve, reject) => {
+        filesystem.readFile(path, 'utf8', (error, content) => {
+            if (error) {
+              reject(error);
+            }
+            resolve(content);
+          }
+        );
+      });
+    },
+    /**
+     * @param {string} path
+     * @param {Buffer} data
+     * @returns {Promise}
+     */
+    writeFile: function (path, data) {
+      return new Promise((resolve, reject) => {
+        filesystem.writeFile(path, data, (error, result) => {
           if (error) {
             reject(error);
           }
           resolve(result);
         });
-      };
+      });
     }
   };
 
   let erp = new DataSource();
   Promise
     .all([
-      new Promise(Promises.createKeypair),
-      new Promise(Promises.readOAuth2ClientSQLFile)
+      promises.generateRSAKeyPair(2048, 65537),
+      promises.readFile(
+        '{{ directory }}/sql/oauth2client.sql'.replace('{{ directory }}', __dirname)
+      )
     ])
     .then((results) => {
       let keypair = results[0];
@@ -118,15 +128,26 @@
           script.iss,
           forge.pki.publicKeyToPem(keypair.publicKey)
         ])),
-        new Promise(Promises.saveP12File(keypair.privateKey))
+        promises.writeFile(
+          script.path,
+          new Buffer(
+            new Buffer(
+              forge.asn1.toDer(
+                forge.pkcs12.toPkcs12Asn1(keypair.privateKey, null, 'notasecret')
+              ).getBytes(),
+              'binary'
+            ),
+            'base64'
+          )
+        )
       ]);
     })
     .then(() => {
       return erp.execute(new Query([
-        "INSERT INTO xdruple.xd_site (xd_site_name, xd_site_url)",
-        "VALUES ($1, $2)",
-        "ON CONFLICT (xd_site_name) DO UPDATE",
-        "SET xd_site_name = $1, xd_site_url = $2"
+        'INSERT INTO xdruple.xd_site (xd_site_name, xd_site_url)',
+        'VALUES ($1, $2)',
+        'ON CONFLICT (xd_site_name) DO UPDATE',
+        'SET xd_site_name = $1, xd_site_url = $2'
       ], [
         script.application,
         script.application
